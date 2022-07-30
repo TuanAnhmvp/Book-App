6770002199c639a6812ba59545f4c953ac688f45
tuana <https://github.com/TuanAnhmvp/Android-Studio>
2022-07-30 21:04:47 +0700

    Add book to favorite.Remove book from favorite

--- app/src/main/java/com/example/bookapp/PdfDetailActivity.kt
+++ app/src/main/java/com/example/bookapp/PdfDetailActivity.kt
@@ -12,6 +12,7 @@ import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import com.example.bookapp.databinding.ActivityPdfDetailBinding
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
@@ -24,6 +25,9 @@ class PdfDetailActivity : AppCompatActivity() {
         //TAG
         const val TAG = "BOOK_DETAILS_TAG"
     }
+
+    //firebase
+    private lateinit var firebaseAuth: FirebaseAuth
 
     //Progress dialog
     private lateinit var progressDialog: ProgressDialog
@@ -38,6 +42,9 @@ class PdfDetailActivity : AppCompatActivity() {
     private var bookTitle = ""
     private var bookUrl = ""
 
+    //hold a boolean value
+    private var isInMyFavorite = false
+
 
 
 
@@ -46,13 +53,24 @@ class PdfDetailActivity : AppCompatActivity() {
         binding = ActivityPdfDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //get book id from intent
+        bookId = intent.getStringExtra("bookId")!!
+
+        //init firebase auth
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        //
+        if (firebaseAuth.currentUser != null){
+            //user is logged in , check if book is in fav or not
+            checkIsFavorite()
+        }
+
         //init progress bar
         progressDialog = ProgressDialog(this)
         progressDialog.setTitle("Please wait...")
         progressDialog.setCanceledOnTouchOutside(false)
 
-        //get book id from intent
-        bookId = intent.getStringExtra("bookId")!!
+
 
         //handle back button click
         binding.backBtn.setOnClickListener {
@@ -70,6 +88,30 @@ class PdfDetailActivity : AppCompatActivity() {
             val intent = Intent(this, PdfViewActivity::class.java)
             intent.putExtra("bookId", bookId)
             startActivity(intent)
+        }
+
+        //handle click, add,remove favorire
+        binding.favoriteBtn.setOnClickListener {
+            //we can add only if user is logged in
+            //check if user is logged in or not
+            if (firebaseAuth.currentUser == null){
+                //user not logged in , cant do favorite functionality
+                Toast.makeText(this, "You're not logged in", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                //user is logged in, can do favorite functionality
+                if (isInMyFavorite){
+                    //already in fav, remove
+                    remoFromFavorite()
+
+                }
+                else{
+                    //not in fav. add
+                        addToFavorite()
+                }
+            }
+
+
         }
 
         //handle click, download book/pdf
@@ -233,5 +275,87 @@ class PdfDetailActivity : AppCompatActivity() {
 
                 }
             })
+    }
+
+    private fun checkIsFavorite(){
+        Log.d(TAG, "checkIsFavorite: Checking if book is in fav or not")
+
+        val ref = FirebaseDatabase.getInstance().getReference("Users")
+        ref.child(firebaseAuth.uid!!).child("Favorites").child(bookId)
+            .addValueEventListener(object : ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    isInMyFavorite = snapshot.exists()
+                    if (isInMyFavorite){
+                        //available in favorite
+                        //set drawable top icon
+                        Log.d(TAG, "onDataChange: available in favorite")
+                        binding.favoriteBtn.setCompoundDrawablesRelativeWithIntrinsicBounds(0, R.drawable.ic_baseline_favorite_white, 0, 0)
+                        binding.favoriteBtn.text = "Remove Favorite"
+
+                    }
+                    else{
+                        //not available in favorite
+                        //set drawable top icon
+                        Log.d(TAG, "onDataChange: not available in favorite")
+                        binding.favoriteBtn.setCompoundDrawablesRelativeWithIntrinsicBounds(0, R.drawable.ic_favorite_white, 0, 0)
+                        binding.favoriteBtn.text = "Add Favorite"
+
+                    }
+
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+
+
+                }
+            })
+
+    }
+
+    private fun addToFavorite(){
+        Log.d(TAG, "addToFavorite: Adding to fav")
+
+        val timestamp = System.currentTimeMillis()
+
+        //setup data to add in db
+        val hashMap = HashMap<String, Any>()
+        hashMap["bookId"] = bookId
+        hashMap["timestamp"] = timestamp
+
+        //save to db
+        val ref = FirebaseDatabase.getInstance().getReference("Users")
+        ref.child(firebaseAuth.uid!!).child("Favorites").child(bookId)
+            .setValue(hashMap)
+            .addOnSuccessListener {
+                //added to fav
+                Log.d(TAG, "addToFavorite: Added to fav")
+                Toast.makeText(this, "Added to favorite",Toast.LENGTH_SHORT).show()
+
+            }
+            .addOnFailureListener { e->
+                //failed to add to fav
+                Log.d(TAG, "addToFavorite: Failed to add to fav due to ${e.message}")
+                Toast.makeText(this, "Failed to add to fav due to ${e.message}",Toast.LENGTH_SHORT).show()
+
+
+            }
+    }
+
+    private fun remoFromFavorite(){
+        Log.d(TAG, "removeFromFavorite: Removing from fav")
+
+        //database ref
+        val ref = FirebaseDatabase.getInstance().getReference("Users")
+        ref.child(firebaseAuth.uid!!).child("Favorites").child(bookId)
+            .removeValue()
+            .addOnSuccessListener {
+                Log.d(TAG, "removeFromFavorite: Removed from fav")
+                Toast.makeText(this, "Removed from favorite",Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener { e->
+                Log.d(TAG, "removeFromFavorite: Failed to remove to fav due to ${e.message}")
+                Toast.makeText(this, "Failed to remove to fav due to ${e.message}",Toast.LENGTH_SHORT).show()
+
+            }
     }
 }
